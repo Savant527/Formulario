d40c1d2473907e5af82ac7da8edc59ead2b609ea
Savant527 <jhonsebastiannavas@gmail.com>
2021-11-16 21:24:47 -0500

    version3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
-function enviar_formulario(){
-   document.formulario.submit()
-}
+
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', e =>{
+    e.preventDefault();
+    const nombre = formulario['nombre'].value;
+    const apellido = formulario['apellido'].value;
+    const tipoDoc = formulario['tipoDocumento'].value;
+    const documento = formulario['documento'].value;
+    const correo = formulario['correo'].value;
+    const observaciones = formulario['observaciones'].value;
+    
+    console.log(nombre, apellido, tipoDoc, documento, correo, observaciones);
+})
